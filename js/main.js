a71f124163a1bd8de6c6a1a9287c98565526bf45
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2023-07-31 17:16:43 +0000

    Add Js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log('Js Work');
+// Recuper le nom
+
+const nameElement = document.getElementById('name');
+console.log(nameElement.value);
+
+// Rcuperer un checkbox
+const checkboxElement = document.getElementById('conditions');
+console.log(checkboxElement.checked);
+
+// Recuper les elements radio
+const contacts = document.querySelectorAll('input[type=radio]');
+
+for (let index = 0; index < contacts.length; index++) {
+	if (contacts[index].checked) {
+		console.log(contacts[index].value);
+	}
+}
